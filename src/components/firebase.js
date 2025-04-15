30f01863fe138fb6fafcbb3dd68105f8928005ca
CosZmo77 <syedsaadahmed77@gmail.com>
2025-04-16 01:32:21 +0530

    Firebase API key was Public so made changes and Created ENV File

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC22ewUwHkPTsN4gv3QHc_Y_DeuPiQaawA",
-  authDomain: "medicalshala-new01.firebaseapp.com",
-  projectId: "medicalshala-new01",
-  storageBucket: "medicalshala-new01.firebasestorage.app",
-  messagingSenderId: "711967624299",
-  appId: "1:711967624299:web:cdbefea2231276a787b52e",
-  measurementId: "G-Q4HKVMX7RQ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
